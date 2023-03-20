b880df63b000959805471a6349fcbcd4cc47dbd9
unknown <alek.simonovski02@gmail.com>
2023-03-20 14:44:34 +0100

    Added comments to code.

--- src/zadaca/zadaca2.java
+++ src/zadaca/zadaca2.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class zadaca2 {
 
 	public static void main(String[] args) {
-		int k, a, b, c;
-		Scanner tastatura = new Scanner(System.in);
-		k = tastatura.nextInt();
+		int k, a, b, c; ///Initializing variables k, a, b and c.
+		Scanner tastatura = new Scanner(System.in);		///Creating a scanner for user input.
+		k = tastatura.nextInt();	///User inputs integer values for each variable
 		a = tastatura.nextInt();
 		b = tastatura.nextInt();
 		c = tastatura.nextInt();
-		if(a<=5 && b<a-c) {
+		if(a<=5 && b<a-c) {			///Conditional structure if.
 			k=5;
 		}
-		System.out.println(k);
+		System.out.println(k);		///Printing value of k.
 		
 			
 		}
