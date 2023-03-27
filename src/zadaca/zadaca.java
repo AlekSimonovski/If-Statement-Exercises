3461acf0f5986d8e580b59fefaabb687a9dfcc7e
unknown <alek.simonovski02@gmail.com>
2023-03-27 15:13:43 +0200

    Added changes to code.

--- src/zadaca/zadaca.java
+++ src/zadaca/zadaca.java
@@ -6,11 +6,11 @@ public class zadaca {
 	public static void main(String[] args) {
 		int k;
 		Scanner tastatura = new Scanner(System.in);
-		k = tastatura.nextInt();
-		if(k>6) {
+		k = tastatura.nextInt();		///Se vnesuva korisnicka vrednost na promenliva K.
+		if(k>6) {						///Dokolku K e pogolemo od 6, se pecati K.
 			System.out.println(k);
 		}
-		else {
+		if(k<6) {						///Dokolku vrednosta na K e pomala od 6, se pecati slednata poraka.
 			System.out.println("Vrednosta na K e pomala od 6.");
 		}
 	}
